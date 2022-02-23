d28c06cc39fc1af198ca7befec39ab797e5912c2
SivaganeshAndhavarapu <100190176+SivaganeshAndhavarapu@users.noreply.github.com>
2022-02-23 10:41:32 +0530

    Add files via upload

---
+++ feat_utils.py
@@ -0,0 +1,33 @@
+import os
+import pathlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import tensorflow as tf
+
+def decode_audio(audio_binary):
+  audio, _ = tf.audio.decode_wav(contents=audio_binary)
+  return tf.squeeze(audio, axis=-1)
+
+def get_label(file_path):
+  parts = tf.strings.split(input=file_path, sep=os.path.sep)
+  return parts[-2]
+
+def get_waveform_and_label(file_path):
+  label = get_label(file_path)
+  audio_binary = tf.io.read_file(file_path)
+  waveform = decode_audio(audio_binary)
+  return waveform, label
+
+def get_spectrogram(waveform):
+  input_len = 16000
+  waveform = waveform[:input_len]
+  zero_padding = tf.zeros([16000] - tf.shape(waveform), dtype=tf.float32)
+  waveform = tf.cast(waveform, dtype=tf.float32)
+  equal_length = tf.concat([waveform, zero_padding], 0)
+  spectrogram = tf.signal.stft( equal_length, frame_length=255, frame_step=128)
+  spectrogram = tf.abs(spectrogram)
+  spectrogram = spectrogram[..., tf.newaxis]
+  return spectrogram
+
